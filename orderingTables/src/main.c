dc60f1474c502dc3ecc3af3ca3d1fd7cb47f4636
John Convertino <electrobs@gmail.com>
2016-10-08 11:15:50 -0400

    added basics for orderingTable and sprite examples, main is just a copy of controller till I sit down and alter the code

---
+++ orderingTables/src/main.c
@@ -0,0 +1,182 @@
+/*
+*/
+
+#include <stdlib.h>
+#include <libgte.h>
+#include <libgpu.h>
+#include <libgs.h>
+#include <libpad.h>
+//#include <libetc.h>
+
+#define SCREEN_WIDTH  320 // screen width
+#define	SCREEN_HEIGHT 240 // screen height
+
+u_long __ramsize   = 0x00200000; // force 2 megabytes of RAM
+u_long __stacksize = 0x00004000; // force 16 kilobytes of stack
+
+void graphics();
+void display(POLY_F4 *f4);
+
+
+static u_char g_pad[2][34];
+
+struct
+{
+	u_char status;
+	u_char type:4;
+	u_char recvSize:4;
+	
+	union
+	{
+		struct
+		{
+			u_char left:1;
+			u_char down:1;
+			u_char right:1;
+			u_char up:1;
+			u_char start:1;
+			u_char NA1:1;
+			u_char NA2:1;
+			u_char select:1;
+		} bit;
+		u_char byte;
+		
+	}first;
+	
+	union
+	{
+		struct
+		{
+			u_char square:1;
+			u_char ex:1;
+			u_char circle:1;
+			u_char triangle:1;
+			u_char R1:1;
+			u_char L1:1;
+			u_char R2:1;
+			u_char L2:1;
+		} bit;
+		u_char byte;
+		
+	}second;
+} g_pad0;
+
+int main() 
+{
+	POLY_F4 f4;
+	int prevTime = 0;
+	
+	graphics(); // setup the graphics (seen below)
+	
+	PadInitDirect(g_pad[0], g_pad[1]);
+	//PadInitDirect(gpad[0], gpad[1]);
+	PadStartCom();
+	
+	SetPolyF4(&f4);
+	/*initialize primitive*/
+	setRGB0(&f4, 0, 0, 255);
+	/*R,G,B = 0, 0, 255*/
+	setXY4(&f4, 0, 0, 50, 0, 0, 50, 50, 50);
+	/*execute primitive*/
+		
+	while (1) // draw and display forever
+	{
+		switch((g_pad[0][2] << 8) | g_pad[0][3])
+		{
+			case 0xFFBF:
+				if(prevTime == 0 || ((VSync(-1) - prevTime) > 60))
+				{
+					f4.r0 = rand() % 256;
+					f4.g0 = rand() % 256;
+					f4.b0 = rand() % 256;
+					prevTime = VSync(-1);
+				}
+				break;
+			case 0xEFFF:
+				f4.y0 -= 1;
+				f4.y1 -= 1;
+				f4.y2 -= 1;
+				f4.y3 -= 1;
+				if(f4.y0 < 0)
+				{
+					f4.y0 = SCREEN_HEIGHT - 50;
+					f4.y1 = SCREEN_HEIGHT - 50;
+					f4.y2 = SCREEN_HEIGHT;
+					f4.y3 = SCREEN_HEIGHT;
+				}
+				break;
+			case 0xDFFF:
+				f4.x0 += 1;
+				f4.x1 += 1;
+				f4.x2 += 1;
+				f4.x3 += 1;
+				if(f4.x1 > SCREEN_WIDTH)
+				{
+					f4.x0 = 0;
+					f4.x1 = 50;
+					f4.x2 = 0;
+					f4.x3 = 50;
+				}
+				break;
+			case 0xBFFF:
+				f4.y0 += 1;
+				f4.y1 += 1;
+				f4.y2 += 1;
+				f4.y3 += 1;
+				if(f4.y2 > SCREEN_HEIGHT)
+				{
+					f4.y0 = 0;
+					f4.y1 = 0;
+					f4.y2 = 50;
+					f4.y3 = 50;
+				}
+				break;
+			case 0x7FFF:
+				f4.x0 -= 1;
+				f4.x1 -= 1;
+				f4.x2 -= 1;
+				f4.x3 -= 1;
+				if(f4.x0 < 0)
+				{
+					f4.x0 = SCREEN_WIDTH - 50;
+					f4.x1 = SCREEN_WIDTH;
+					f4.x2 = SCREEN_WIDTH - 50;
+					f4.x3 = SCREEN_WIDTH;
+				}
+				break;
+			default:
+				break;
+		}
+		
+		display(&f4);
+	}
+
+	return 0;
+}
+
+void graphics()
+{
+	// within the BIOS, if the address 0xBFC7FF52 equals 'E', set it as PAL (1). Otherwise, set it as NTSC (0)
+	switch(*(char *)0xbfc7ff52=='E')
+	{
+		case 'E':
+			SetVideoMode(1); 
+			break;
+		default:
+			SetVideoMode(0); 
+			break;	
+	}
+	
+	GsInitGraph(SCREEN_WIDTH, SCREEN_HEIGHT, GsINTER|GsOFSGPU, 1, 0); // set the graphics mode resolutions. You may also try using 'GsNONINTER' (read LIBOVR46.PDF in PSYQ/DOCS for detailed information)
+	GsDefDispBuff(0, 0, 0, SCREEN_HEIGHT); // set the top left coordinates of the two buffers in video memory
+}
+
+
+void display(POLY_F4 *f4)
+{
+	GsClearDispArea(0,0,0);
+	DrawPrim(f4);
+	DrawSync(0); // wait for all drawing to finish
+	VSync(0); // wait for v_blank interrupt
+	GsSwapDispBuff(); // flip the double buffers
+}
